40ae3f1afff8e8c406a9258856ab83bff04e9a86
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2026-04-04 19:06:56 +0530

    fix: fixed cookie bug

--- controllers/user.js
+++ controllers/user.js
@@ -59,7 +59,7 @@ async function loginUser(req, res) {
 
 
 
-    return res.cookie('token', token, {secure: true, sameSite: 'strict', maxAge: 7 * 24 * 60 * 60 * 1000 }).status(200).json({ message: 'Login successful' });
+    return res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 7 * 24 * 60 * 60 * 1000 }).status(200).json({ message: 'Login successful' });
   } catch (error) {
     return res.status(500).json({ error: 'Failed to login user' });
   }
